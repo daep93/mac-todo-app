49ee7eac0fde819b29b72771b3d492a771aa4075
daep93 <eoy1313@gmail.com>
2022-12-03 14:55:00 +0900

    feat: add fortawesome and prettier, eslint

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -3,10 +3,21 @@ import { createPinia } from "pinia";
 import "./index.css";
 import App from "./App.vue";
 import router from "./router";
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(faSquarePlus);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
