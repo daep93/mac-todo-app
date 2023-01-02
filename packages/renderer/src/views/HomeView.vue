8c6a0bbc93444bc8c5791c96e98918a925133b24
daep93 <eoy1313@gmail.com>
2023-01-02 21:51:51 +0900

    feat: add timer

--- packages/renderer/src/views/HomeView.vue
+++ packages/renderer/src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, onUnmounted } from "vue";
+import dayjs from "dayjs";
 import TodoInput from "../components/TodoInput.vue";
 import TodoList from "../components/TodoList.vue";
 import { getAll, getAllKeys, add, del } from "../idb";
 type Todo = { key: string; text: string; start: string; end: string };
 const items = ref<Todo[]>([]);
+const timers: ReturnType<typeof setTimeout>[] = [];
+
 onMounted(async () => {
   const keys = await getAllKeys();
   const values = await getAll();
-  items.value = values.reduce((acc, value, idx) => [...acc, { key: keys[idx], ...value }], []);
+  items.value = values.reduce((acc, value, idx) => [...acc, { key: keys[idx].toString(), ...value }], []);
+  const now = dayjs();
+  values.forEach((value) => {
+    const end = dayjs(value.end);
+    if (!now.isBefore(end)) return;
+    console.log(value.todo, end.diff(now));
+    const timer = setTimeout(() => {
+      new Notification("end todo", { body: `${value.text} is done` });
+    }, end.diff(now));
+    timers.push(timer);
+  });
+});
+onUnmounted(() => {
+  timers.forEach((timer) => clearTimeout(timer));
 });
 const registTodo = async (todo: { start: string; end: string; text: string }) => {
   const key = (await add(todo)) as string;
   items.value = [...items.value, { key, ...todo }];
+  const timer = setTimeout(() => {
+    new Notification("end todo", { body: `${todo.text} is done` });
+  }, dayjs(todo.end).diff(dayjs()));
+  timers.push(timer);
   new Notification("create new todo", { body: `'${todo.text}' is created` });
 };
 const deleteTodo = (key: string) => {
